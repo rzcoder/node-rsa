c86d46f976645b70678143bd53ea50da09131f8b
rzcoder <rzcoder@gmail.com>
2015-03-03 02:49:00 +0500

    fix encrypt engine choice bug

--- src/encryptEngines/encryptEngines.js
+++ src/encryptEngines/encryptEngines.js
@@ -2,10 +2,8 @@ var crypt = require('crypto');
 
 module.exports = {
     getEngine: function (keyPair, options) {
-        var engine;
-        if (options.environment === 'browser') {
-            engine = require('./js.js');
-        } else {
+        var engine = require('./js.js');
+        if (options.environment === 'node') {
             if (typeof crypt.publicEncrypt === 'function' && typeof crypt.privateDecrypt === 'function') {
                 if (typeof crypt.privateEncrypt === 'function' && typeof crypt.publicDecrypt === 'function') {
                     engine = require('./io.js');
